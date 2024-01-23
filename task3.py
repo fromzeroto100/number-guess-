76c12b44112329a2066ff52279caea10401a570a
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-01-23 22:38:16 +0100

    last

--- task3.py
+++ task3.py
@@ -1,25 +1,33 @@
 import random
 import os
 
+def check_answer():
+    if user_guess > answer:
+        print("Too high!")
+    elif user_guess < answer:
+        print("Too low!")    
+    else:
+        print(f"You got it. The answer was {answer}")
+
+easy_level = 10
+dif_level = 5
+def set_difficulty():
+    level = input("Choose a difficulty. Type 'easy' or 'hard': ")        
+    if level == easy:
+        return easy_level
+    else:
+        return dif_level    
+
+
+print("Welcome to the number guess game")
+print("Guess a number between 1 and 100")
+answer = randint(1, 100)
+
+user_guess = int(input("Input your guess: "))
+
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-print("Welcome to the number guess game")
-
-easy_game = [1, 2, 3, 4, 5]
-hard_game = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-number = int()
-def guess(number):
-    number = random.randint()
-    return
-
-user_guess = int(input("Input your guess: "))
 
 
-def check_guess():
-    if user_guess > guess:
-        print("Too high!")
-    elif user_guess < guess:
-        print("Too low!")    
