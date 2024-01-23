fc3a7094b320f157b6bc7ea4491191cfdbcfbcf9
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-01-23 22:24:58 +0100

    new1

--- task3.py
+++ task3.py
@@ -1,1 +1,25 @@
+import random
+import os
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
 print("Welcome to the number guess game")
+
+easy_game = [1, 2, 3, 4, 5]
+hard_game = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+number = int()
+def guess(number):
+    number = random.randint()
+    return
+
+user_guess = int(input("Input your guess: "))
+
+
+def check_guess():
+    if user_guess > guess:
+        print("Too high!")
+    elif user_guess < guess:
+        print("Too low!")    
